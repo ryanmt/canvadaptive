6267f2708505450eae4bb1e55a4dc91aee099354
Ryan Taylor <rtaylor@instructure.com>
2014-10-16 16:56:53 -0600

    Button one step closer

--- app/assets/javascripts/components/TestIndex.js.jsx
+++ app/assets/javascripts/components/TestIndex.js.jsx
@@ -4,22 +4,21 @@ var TestIndex = React.createClass({
 
     propTypes: {
     },
-    onClick: function(element, e) {
-      console.log(element)
-      console.log(e)
+    onClick: function(e) {
+      console.log(e.target)
       console.log(test.url)
     },
 
     render: function () {
-          var rows = []
-          this.props.tests.forEach(function(test) {
-            rows.push(
+          var rows = this.props.tests.map(function(test) {
+            return (
               <div z="1" className="testTableRow x-card host" key={test.id}>
                 <label className="CanvadaptiveTestIndex__Title">{test.title}</label>
-                <button onClick={this.onClick()} type="button" className="CanvadaptiveTestIndex__StartButton paper-button raisedButton">Start this test</button>
+                <button onClick={this.onClick} type="button" className="CanvadaptiveTestIndex__StartButton paper-button raisedButton">Start this test</button>
                 <div className="paper-shadow-bottom"></div>
               </div>)
           });
+          
           return (
         <div className="testTable card-holder">
           <div className="card-contents">
